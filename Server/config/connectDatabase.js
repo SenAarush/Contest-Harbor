7215729e62a3575df028650f6aa6426b072457ba
SenAarush <senaarush@outlook.com>
2024-05-19 20:13:37 +0530

    preparing for nextjs

--- Server/config/connectDatabase.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const db = process.env.DATABASE;
-
-mongoose.connect(db).then(() => {
-    console.log("Database connection successfull!")
-}).catch((err) => console.log("Database connection unsuccessfull"))
